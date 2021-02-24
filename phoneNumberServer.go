9d3fd120c21345a4a0445ad046f153e4d9b477d5
leeyou01 <lee.y.y.you@outlook.com>
2021-02-24 23:24:48 +1300

    updated phone number validator server

--- phoneNumberServer.go
+++ phoneNumberServer.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"net/http"
+
+	"phoneNumber/logger"
+	"phoneNumber/webService"
 )
 
 // A phone number validator server for parsing and or verifying international phone numbers
 func main() {
 	const SERVER_ADDR string = ":8080"
 	logger.Info.Println("\n  PHONE NUMBER VALIDATOR SERVER IS RUNNING... @", SERVER_ADDR)
+	err := http.ListenAndServe(SERVER_ADDR, webService.GetRouter())
 
+	if err != nil {
+		logger.Info.Println(err)
+	}
 }
